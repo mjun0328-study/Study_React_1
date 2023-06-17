173ef8102bfd7ec5a62bb7741a062a2c627df3bb
etimel.dev <etimeldev@gmail.com>
2023-06-17 18:24:44 +0900

    6.5 React Router

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
